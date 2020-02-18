13719eae4e2fc72d0498ddc3754e77bee123672a
jmccutcheon16 <49525814+jmccutcheon16@users.noreply.github.com>
2020-02-18 09:41:25 -0500

    feb 18 classwork loops counting

--- inclass/feb13_burger_nav/script.js
+++ inclass/feb13_burger_nav/script.js
@@ -33,6 +33,16 @@ function isBlank(data, errorSpanId){
     return false;
 }
 
+function isNotValidNum(data, errorSpanId){
+    if(isNaN(data)) {
+        let errorSpan = document.getElementById(errorSpanId);
+        errorSpan.classList.remove("hidden");
+        return true;
+    }
+
+    return false;
+}
+
 function getEmoji(emotion){
     const emoCI = emotion.toLowerCase();
 
@@ -59,9 +69,87 @@ function toggleNav() {
     navItems.classList.toggle("hidden");
 }
 
+function displayCount()
+{
+
+    let startNum = parseInt(document.getElementById("txt-start").value);
+    let endNum = parseInt(document.getElementById("txt-end").value);
+    //let resultDiv = document.getElementById("ex3-result");
+
+    // Clear both error spns by setting html to blank
+    document.getElementById("error-start").classList.add("hidden");
+    document.getElementById("error-end").classList.add("hidden"); 
+    
+    // If either number is not a number show error appropriately
+    if(isNotValidNum(startNum, "error-start") | isNotValidNum(endNum, "error-end"))
+    {
+        return;
+    } 
+
+    //If endNum is <= startNum show an error appropriately
+    if(startNum>=endNum)
+    {
+        resultDiv.innerHTML = "End number must be larger than start number!";
+        return;
+    }
+
+    // Version 1
+    /*
+    resultDiv.innerHTML = "Counting:<ul>";
+    for(let i=startNum; i<=endNum; i++)
+    {
+        resultDiv.innerHTML += `<li>${i}</li>`;
+    }
+    resultDiv.innerHTML += "</ul>All done";
+    */
+
+    //Version 2
+    let h3Elem = document.createElement("h3");
+    h3Elem.textContent = "Counting...";
+    let ulElem = document.createElement("ul");
+    btnCount.after(ulElem);
+    for(let i=startNum; i<=endNum; i++)
+    {
+        let liElem = document.createElement("li");
+        liElem.textContent = i;
+        ulElem.after(liElem);
+    }
+}
+
+function displayStuff()
+{
+    let cookieTitle = document.createElement("h3");
+    let ulElem = document.createElement("ul");
+    let endTitle = document.createElement("p");
+
+    cookieTitle.textContent = "I Love Cookies";
+    endTitle.textContent = "Cookie Time";
+    ulElem.classList.add("cookie-list");
+
+    // Add items to the Document Object Model
+    this.after(cookieTitle);
+    cookieTitle.after(ulElem);
+    ulElem.after(endTitle);
+
+    // Populate the numerical list
+    for(let i=5; i>=1; i--)
+    {
+        let liElem = document.createElement("li");
+        liElem.textContent = i;
+        ulElem.append(liElem);
+
+    }
+}
+
 const btnDisplay = document.getElementById("btn-display");
 btnDisplay.onclick = displayEmotion;
 
 const navToggle = document.getElementById("nav-toggle");
 navToggle.onclick = toggleNav;
+
+const btnCount = document.getElementById("btn-go");
+btnCount.onclick = displayCount;
+
+const btnStuff = document.getElementById("do-stuff");
+btnStuff.onclick = displayStuff;
 
