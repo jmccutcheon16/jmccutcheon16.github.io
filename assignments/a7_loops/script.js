b740c3f76aaad89116417c65c8cf3e0d90559295
jmccutcheon16 <49525814+jmccutcheon16@users.noreply.github.com>
2020-02-27 10:54:06 -0500

    finished assignment 7

--- assignments/a7_loops/script.js
+++ assignments/a7_loops/script.js
@@ -1,9 +1,27 @@
-function setOpacity(imageid, opacity) {
-    var s = document.getElementById(imageid).style;
-    s.opacity = opacity / 100;
-    s.MozOpacity = opacity / 100;
-    s.KhtmlOpacity = opacity / 100;
-    s.filter = 'alpha(opacity=' + opacity + ')';
+let count = 0;
+function moveMan() {
+    let man = document.getElementById('man');
+    count += 20;
+    //man.style.marginLeft += 20 + 'px';
+    man.style.marginLeft = count + 'px';
+
+    console.log('working');
 }
 
-setOpacity('man', 50);
+let totalFunds = 0;
+let funds = document.getElementById('funds-raised-input');
+let fundsBar = document.getElementsByClassName('bar');
+function fundRaising() {
+    if (document.getElementById('display-funds')) {
+        totalFunds += funds;
+    }
+    console.log(totalFunds);
+    fundsBar.style.append(
+        'background: linear-gradient(0deg,rgba(255, 0, 0, 1) ' +
+            funds +
+            '%,rgba(255, 0, 0, 1) 0%,rgba(255, 255, 255, 1) 0%,rgba(255, 255, 255, 1) 0%)'
+    );
+}
+
+man = document.getElementById('man');
+man.onclick = moveMan;
