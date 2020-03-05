fe606b183dd407c21bcb49806182d7dc4ce6c9ab
jmccutcheon16 <49525814+jmccutcheon16@users.noreply.github.com>
2020-03-05 11:43:18 -0500

    A8

--- assignments/a8_arrays/script.js
+++ assignments/a8_arrays/script.js
@@ -1,59 +1,33 @@
-class Dog {
+// QUOTES
+let quotes = [
+    'Love For All, Hatred For None.',
+    'Change the world by being yourself.',
+    'Every moment is a fresh beginning',
+    'Never regret anything that made you smile',
+    'Die with memories, not dreams.'
+];
 
-    constructor(title, breed, color, age, size, pic){
-        this.title = title;
-        this.breed = breed;
-        this.color = color;
-        this.age = age;
-        this.size = size;
-        this.pic = pic;
+// TICK TIMER
+var ticking = window.setInterval(tick, 2000);
+let currentIndex = -1;
+function tick() {
+    currentIndex++;
+    if (currentIndex >= quotes.length) {
+        currentIndex = 0;
     }
-
-    get details(){
-        return `${this.title} is a ${this.breed}`;
-    }
-
-    get item(){
-        let dogSection = document.createElement("section");
-
-        //create picture
-        let imgElem = document.createElement("img");
-        imgElem.src= `images/${this.pic}`;
-        dogSection.append(imgElem);
-
-        //create title
-        let h3Elem = document.createElement("h3");
-        h3Elem.innerText = this.title;
-        dogSection.append(h3Elem);
-
-        //create list
-        let ulElem = document.createElement("ul");
-        dogSection.append(ulElem);
-        let liBreedElem = document.createElement("li");
-        liBreedElem.innerText = `Breed: ${this.breed}`;
-        ulElem.append(liBreedElem);
-
-        let liColorElem = document.createElement("li");
-        liColorElem.innerText = `Color: ${this.color}`;
-        ulElem.append(liColorElem);
-
-        let liAgeElem = document.createElement("li");
-        liAgeElem.innerText = `Age: ${this.age}`;
-        ulElem.append(liAgeElem);
-
-        let liSizeElem = document.createElement("li");
-        liSizeElem.innerText = `Breed: ${this.size}`;
-        ulElem.append(liSizeElem);
-
-        return dogSection;
-    }
-
+    console.log(quotes[currentIndex]);
+    document.getElementById('quotes-results').innerHTML = '';
+    document.getElementById('quotes-results').innerHTML = quotes[currentIndex];
 }
 
-//After the DOM has been loaded
-//After all the HTML elemnts have been rendered
-window.onload = function(){
-    let myDog = new Dog("Tipsy", "Yorkie", "brown", 5, "small", "yorkie.jpg");
-    let dogListDiv = document.getElementById("dog-list");
-    dogListDiv.append(myDog.item);
+function showRainbow() {
+    let pRainbow = document.createElement('p');
+    let pRainbowInnerHTML = pRainbow.innerHTML;
+    let addText = (pRainbowInnerHTML += 'k');
+    pRainbowInnerHTML += 'k';
+    let log = console.log('working');
+    setTimeout(addText, 1000);
 }
+
+let btnRainbow = document.getElementById('btn-rainbow');
+btnRainbow.onclick = showRainbow;
