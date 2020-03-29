1d5bcaf6bd5fd05633cb10b540df268eedda09e1
jmccutcheon16 <49525814+jmccutcheon16@users.noreply.github.com>
2020-03-29 14:59:32 -0400

    json

--- inclass/mar29_JSON/script.js
+++ inclass/mar29_JSON/script.js
@@ -1,5 +1,38 @@
-function showShoes() {
+async function showShoes() {
+    let response = await fetch(
+        "https://jmccutcheon16.github.io/json/shoes.json"
+    );
+    let shoesJSON = await response.json();
     let shoeDiv = document.getElementById("shoes-section");
+    // Loot through the list of shoes in the json file
+    for (i in shoesJSON) {
+        let shoe = shoesJSON[i];
+        shoeDiv.append(getShoeItem(shoe));
+    }
+}
+
+function getShoeItem(shoe) {
+    let shoeSection = document.createElement("section");
+    let h3Elem = document.createElement("h3");
+    h3Elem.innerText = shoe.name;
+    shoeSection.append(h3Elem);
+
+    let ulElem = document.createElement("ul");
+    shoeSection.append(ulElem);
+
+    let liBrand = document.createElement("li");
+    liBrand.innerText = `Brand: ${shoe.brand}`;
+    ulElem.append(liBrand);
+
+    let liPrice = document.createElement("li");
+    liPrice.innerText = `Price: $${shoe.price}`;
+    ulElem.append(liPrice);
+
+    let liReviews = document.createElement("li");
+    liReviews.innerText = `Reviews: ${shoe.reviews.join(", ")}`;
+    ulElem.append(liReviews);
+
+    return shoeSection;
 }
 
 window.onload = function() {
